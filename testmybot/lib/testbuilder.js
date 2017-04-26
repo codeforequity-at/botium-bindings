3113606475947e863395f8d276b7080e4b388723
ftreml <ftreml@gmail.com>
2017-04-26 14:44:08 +0000

    extracting testbuilder to seperate module

---
+++ testmybot/lib/testbuilder.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const log = require('./util/log');
+const convo = require('./convo');
+
+const async = require('async');
+const _ = require('lodash');
+
+function setupTestSuite(testcaseCb, assertCb, failCb, hears, says) {
+  
+  var testcaseFiles = convo.getConvoFilesSync();
+ 
+  testcaseFiles.forEach(function(testcaseFile) {
+    log.info('adding test case file ' + testcaseFile);
+    
+    testcaseCb(testcaseFile, function(testcaseDone) {
+      log.info('running testcase ' + testcaseFile);
+      
+      convo.readConvo(testcaseFile).then(
+        (testcase) => {
+          
+          async.eachSeries(testcase.conversation,
+            (convomsg, convomsgDone) => {
+              
+              if (convomsg.from === 'me') {
+                log.debug(testcase.name + ': hears ' + convomsg.msg);
+                hears(convomsg.msg, convomsg.from, convomsg.channel).then(convomsgDone).catch(convomsgDone);
+              } else if (convomsg.from === 'bot') {
+                log.debug(testcase.name + ': wait for says (channel: ' + convomsg.channel + ')');
+                says(convomsg.channel).then((saysmsg) => {
+                  if (saysmsg.messageText) {
+                    log.debug(testcase.name + ': says ' + saysmsg.messageText);
+
+										var response = saysmsg.messageText.split(/\r?\n/).map((line) => line.trim()).join(' ').trim();
+										var tomatch = convomsg.msg.split(/\r?\n/).map((line) => line.trim()).join(' ').trim();
+                    assertCb(response, tomatch);
+                  } else {
+                    log.debug(testcase.name + ': says ' + JSON.stringify(saysmsg));
+
+                    compareObject(assertCb, failCb, saysmsg, convomsg.msg);
+                  }
+                  convomsgDone();
+                }).catch((err) => {
+                  convomsgDone(err);
+                });
+              }
+            },
+            (err) => {
+              if (err) {
+                log.info(testcase.name + ' failed: ' + err);
+                failCb(err); 
+              }
+              log.info(testcase.name + ' ready, calling done function.');
+              testcaseDone();
+            });
+
+        }, 
+        (err) => {
+          failCb(err);
+        });
+    });
+  });
+}
+
+function compareObject(assertCb, failCb, result, expected) {
+
+  if (expected === null || expected === undefined)
+    return;
+  
+  if (_.isObject(expected)) {
+    _.forOwn(expected, function(value, key) { 
+      
+      if (result.hasOwnProperty(key)) {
+        compareObject(assertCb, failCb, result[key], expected[key]);
+      } else {
+        failCb('missing property: ' + key);
+      }
+    });
+  } else {
+    assertCb(result, expected);  
+  }
+}
+
+module.exports = {
+  setupTestSuite: setupTestSuite
+};
