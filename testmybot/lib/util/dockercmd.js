0b17851d10af4347649645c964cc64a2d4a1158c
ftreml <ftreml@gmail.com>
2017-06-06 15:38:43 +0000

    using Docker mounted volumes to speed up docker container creation

--- testmybot/lib/util/dockercmd.js
+++ testmybot/lib/util/dockercmd.js
@@ -278,7 +278,8 @@ function startContainer(filterCallback) {
           cmdOptions.push(envkey + '=' + envvalue + '');
         });
       }
-
+      cmdOptions.push('-v');
+      cmdOptions.push(process.env.PWD + ':/usr/src/app');
       cmdOptions.push('--name');
       cmdOptions.push(containerSpec.containername);
       cmdOptions.push('--network');
