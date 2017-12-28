c7a10b4b77417d4cea47bd129be90c02493efb7a
ftreml <ftreml@gmail.com>
2017-12-28 14:53:10 +0000

    slack sample

--- samples/slack/skills/sample_events.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(controller) {
-
-    controller.on('user_channel_join,user_group_join', function(bot, message) {
-
-        bot.reply(message, 'Welcome, <@' + message.user + '>');
-
-    });
-
-}
