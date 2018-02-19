ddffba0c483fb6711561de3a016760077fa698f9
ftreml <ftreml@gmail.com>
2018-02-19 11:52:52 +0000

    botkit fix

--- src/helpers/botkit.js
+++ src/helpers/botkit.js
@@ -1,7 +1,6 @@
-const mockery = require('mockery')
 const _ = require('lodash')
 
-const tmb = require('../testmybot')
+const TestMyBot = require('../testmybot')
 const Queue = require('botium-core/src/helpers/Queue')
 
 let controller = null
@@ -12,76 +11,66 @@ let timeoutMillisDefault = 5000
 /**
  * Setup TestMyBot and wire it with Botkit
  */
+TestMyBot.prototype.beforeAll = () => Promise.resolve()
+TestMyBot.prototype.afterEach = () => Promise.resolve()
+TestMyBot.prototype.afterAll = () => Promise.resolve()
+TestMyBot.prototype.hears = (arg) => {
+  let message = null
+  let postback = null
 
-let tmbMock = {
-  beforeAll: () => Promise.resolve(),
-  afterEach: () => Promise.resolve(),
-  afterAll: () => Promise.resolve(),
-  hears: (arg) => {
-    let message = null
-    let postback = null
-
-    if (_.isString(arg)) {
-      message = {
-        text: arg,
-        seq: 1,
-        is_echo: false,
-        mid: 1
-      }
-    } else if (arg.messageText) {
-      message = {
-        text: arg.messageText,
-        seq: 1,
-        is_echo: false,
-        mid: 1
-      }
-    } else if (arg.sourceData && arg.sourceData.postback) {
-      postback = arg.sourceData.postback
-    } else if (arg.sourceData) {
-      message = {
-        text: arg.sourceData,
-        seq: 1,
-        is_echo: false,
-        mid: 1
-      }
-    } else {
-      return Promise.resolve()
+  if (_.isString(arg)) {
+    message = {
+      text: arg,
+      seq: 1,
+      is_echo: false,
+      mid: 1
     }
-
-    controller.handleWebhookPayload(
-      {
-        body: {
-          entry: [
-            {
-              messaging: [
-                {
-                  sender: { id: arg.sender },
-                  recipient: { id: 1 },
-                  timestamp: 1,
-                  message: message,
-                  postback: postback
-                }
-              ]
-            }
-          ]
-        }
-      }, null, bot)
+  } else if (arg.messageText) {
+    message = {
+      text: arg.messageText,
+      seq: 1,
+      is_echo: false,
+      mid: 1
+    }
+  } else if (arg.sourceData && arg.sourceData.postback) {
+    postback = arg.sourceData.postback
+  } else if (arg.sourceData) {
+    message = {
+      text: arg.sourceData,
+      seq: 1,
+      is_echo: false,
+      mid: 1
+    }
+  } else {
     return Promise.resolve()
-  },
-  says: (channel, timeoutMillis) => {
-    return msgqueue.pop(timeoutMillis || timeoutMillisDefault)
   }
-}
-tmbMock = Object.assign(tmb, tmbMock)
 
-mockery.registerMock('testmybot', tmbMock)
-mockery.enable({
-  warnOnReplace: false,
-  warnOnUnregistered: false
-})
+  controller.handleWebhookPayload(
+    {
+      body: {
+        entry: [
+          {
+            messaging: [
+              {
+                sender: { id: arg.sender },
+                recipient: { id: 1 },
+                timestamp: 1,
+                message: message,
+                postback: postback
+              }
+            ]
+          }
+        ]
+      }
+    }, null, bot)
+  return Promise.resolve()
+}
+TestMyBot.prototype.says = (channel, timeoutMillis) => {
+  return msgqueue.pop(timeoutMillis || timeoutMillisDefault)
+}
 
 module.exports.wireWithBotkit = (beforeEachCallback, _timeoutMillisDefault) => {
-  tmb.beforeEach = () => {
+  TestMyBot.prototype.beforeEach = () => {
     msgqueue = new Queue()
     if (_timeoutMillisDefault) {
       timeoutMillisDefault = _timeoutMillisDefault
