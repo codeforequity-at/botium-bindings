30728aba07d5a3d8a4916862f5f9a697d360bef8
tremlfl <florian.treml@drei.com>
2019-11-02 18:26:44 +0100

    Output Botium Error details

--- src/BotiumBindings.js
+++ src/BotiumBindings.js
@@ -1,8 +1,9 @@
 const util = require('util')
 const path = require('path')
+const _ = require('lodash')
 const debug = require('debug')('botium-bindings-main')
 
-const BotDriver = require('botium-core').BotDriver
+const { BotDriver } = require('botium-core')
 
 module.exports = class BotiumBindings {
   constructor ({ botiumConfig, ...args } = {}) {
@@ -71,6 +72,32 @@ module.exports = class BotiumBindings {
     }
   }
 
+  wrapBotiumError (err) {
+    const lines = []
+    lines.push(err.message)
+    if (err.cause && err.cause.context) {
+      const errArr = _.isArray(err.cause.context) ? err.cause.context : [err.cause.context]
+      errArr.forEach(errDetail => {
+        lines.push('########################################')
+        if (errDetail.type === 'asserter') {
+          const segments = []
+          segments.push(`ASSERTION FAILED in ${errDetail.source}${errDetail.subtype ? ` (${errDetail.subtype})` : ''}`)
+          errDetail.cause && errDetail.cause.expected && !errDetail.cause.not && segments.push(` - Expected: "${errDetail.cause.expected}" `)
+          errDetail.cause && errDetail.cause.expected && errDetail.cause.not && segments.push(` - NOT Expected: "${errDetail.cause.expected}" `)
+          errDetail.cause && errDetail.cause.actual && segments.push(` - Actual: "${errDetail.cause.actual}"`)
+          errDetail.cause && !errDetail.cause.actual && segments.push(' - Actual: empty')
+          lines.push(segments.join(''))
+          errDetail.input && errDetail.input.messageText && lines.push(`INPUT: ${errDetail.input.messageText}`)
+        } else if (errDetail.message) {
+          lines.push(`${errDetail.message}`)
+        }
+        lines.push('----------------------------------------')
+        lines.push(JSON.stringify(errDetail))
+      })
+    }
+    return new Error(lines.join('\r\n'))
+  }
+
   setupTestSuite (testcaseCb, assertCb, failCb) {
     if (this.convodirs && this.convodirs.length) {
       this.convodirs.forEach((convodir) => {
@@ -109,7 +136,7 @@ module.exports = class BotiumBindings {
             })
             .catch((err) => {
               debug(`Test Case "${convo.header.name}" failed: ${util.inspect(err)}`)
-              testcaseDone(err)
+              testcaseDone(this.wrapBotiumError(err))
             })
         } else {
           testcaseDone(new Error('Botium Initialization failed. Please see error messages above (enable debug logging).'))
